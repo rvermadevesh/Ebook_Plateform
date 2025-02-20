aa66f9d918edc214c19c89dafc004fa09af151ca
Siddharth Srivastava <143588718+Siddharth4501@users.noreply.github.com>
2025-02-20 16:47:06 +0530

    added some css

--- Client/vite-project/src/pages/EbookMainPage.jsx
+++ Client/vite-project/src/pages/EbookMainPage.jsx
@@ -2,8 +2,12 @@
  
  function EbookMainPage() {
    return (
-     <div className='bg-gray-200'>
-       
+     <div className='bg-gray-200 w-screen h-screen flex items-center'>
+       <div className='border-2 border-gray-600 w-1/2 flex justify-center m-auto min-h-80 bg-slate-200'>
+          <div className='flex mt-24 font-semibold text-2xl'>
+            E Book Portal
+          </div>
+       </div>
      </div>
    )
  }
